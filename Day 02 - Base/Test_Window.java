133c67831b34063a24144b86a61d1eb06b0b56e6
Lee Stemkoski <stemkoski@gmail.com>
2022-08-30 14:20:53 -0400

    Day 2 code

---
+++ Day 02 - Base/Test_Window.java
@@ -0,0 +1,21 @@
+import graphics.core.*;
+
+public class Test_Window extends Base
+{
+    public static void main(String[] args)
+    {
+        Base test = new Test_Window();
+        test.setWindowSize(800, 600);
+        test.run();
+    }
+    
+    public void initialize()
+    {
+        
+    }
+    
+    public void update()
+    {
+        
+    }
+}
